849e43e6092f05506fd7b042fd4454794faf9a4f
alannuari <alannuari372@gmail.com>
2022-03-09 22:28:14 +0700

    merapikan footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <footer className="py-3 w-full bg-slate-300 absolute bottom-0 text-sm sm:text-base">
-            <p className="text-center">
+            <p className="text-center px-2">
                 &#64;2022 Alan Nuari | All data obtained from{' '}
                 <a href="https://github.com/azharimm/food-recipe-api" target="_blank" rel="noreferrer" className="underline text-color-primary">
                     FoodResepAPI
