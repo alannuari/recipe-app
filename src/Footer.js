96565c14223e7e3860bce2d2287cd63e1739f492
alannuari <alannuari372@gmail.com>
2022-03-09 22:10:32 +0700

    menambahkan footer

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+const Footer = () => {
+    return (
+        <footer className="py-3 w-full bg-slate-300 absolute bottom-0">
+            <p className="text-center">
+                &#64;2022 Alan Nuari | All data obtained from{' '}
+                <a href="https://github.com/azharimm/food-recipe-api" target="_blank" rel="noreferrer" className="underline text-color-primary">
+                    FoodResepAPI
+                </a>
+            </p>
+        </footer>
+    );
+};
+
+export default Footer;
--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Footer from './Footer';
 import Loading from './Loading';
 import Recipe from './Recipe';
 import SearchIcon from './search.svg';
@@ -70,42 +71,45 @@ const Home = () => {
     };
 
     return (
-        <div className="min-h-screen bg-slate-200 px-2 py-8 flex items-center flex-col">
-            <h1 className="text-3xl font-bold pb-6">Recipe App</h1>
-            <div className="flex max-w-md w-full bg-color-primary px-4 p-1 sm:py-2 rounded-full">
-                <input
-                    type="text"
-                    onChange={(e) => {
-                        setSearch(e.target.value);
-                    }}
-                    placeholder="Cari Resep Masakan"
-                    className="flex-1 bg-transparent sm:px-4 outline-none mr-4 text-white font-700"
-                />
-                <img
-                    onClick={() => {
-                        setQuery(search);
-                    }}
-                    src={SearchIcon}
-                    alt="search"
-                    width={35}
-                    className="cursor-pointer hover:bg-white p-1 rounded"
-                />
+        <div className="min-h-screen bg-slate-200 relative">
+            <div className="px-2 pt-8 pb-10 flex items-center flex-col">
+                <h1 className="text-3xl font-bold pb-6">Recipe App</h1>
+                <div className="flex max-w-md w-full bg-color-primary px-4 p-1 sm:py-2 rounded-full">
+                    <input
+                        type="text"
+                        onChange={(e) => {
+                            setSearch(e.target.value);
+                        }}
+                        placeholder="Cari Resep Masakan"
+                        className="flex-1 bg-transparent sm:px-4 outline-none mr-4 text-white font-700"
+                    />
+                    <img
+                        onClick={() => {
+                            setQuery(search);
+                        }}
+                        src={SearchIcon}
+                        alt="search"
+                        width={35}
+                        className="cursor-pointer hover:bg-white p-1 rounded"
+                    />
+                </div>
+                {isPending ? (
+                    <Loading />
+                ) : (
+                    <div className="flex flex-wrap justify-center items-center py-6">
+                        {recipes.map((recipe) => (
+                            <Recipe recipe={recipe} key={recipe.id} />
+                        ))}
+                    </div>
+                )}
+                {isEmpty && (
+                    <div className="text-slate-400 text-center">
+                        <h1>Maaf, data tidak ditemukan!.</h1>
+                    </div>
+                )}
+                {error && <div className="text-slate-400">{error}</div>}
             </div>
-            {isPending ? (
-                <Loading />
-            ) : (
-                <div className="flex flex-wrap justify-center items-center py-6">
-                    {recipes.map((recipe) => (
-                        <Recipe recipe={recipe} key={recipe.id} />
-                    ))}
-                </div>
-            )}
-            {isEmpty && (
-                <div className="text-slate-400 text-center">
-                    <h1>Maaf, data tidak ditemukan!.</h1>
-                </div>
-            )}
-            {error && <div className="text-slate-400">{error}</div>}
+            <Footer />
         </div>
     );
 };
