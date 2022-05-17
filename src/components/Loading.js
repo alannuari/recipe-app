46c6b696cb58e5bf4c0c5ef3f84db382f03e6a4d
alannuari <alannuari372@gmail.com>
2022-05-17 19:01:10 +0800

    memperbaiki sintax

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -7,7 +7,6 @@ const Loading = () => {
             <BallTriangle color="blue" height={80} width={80} />
             <h2 className="text-center text-color-secondary text-xl font-semibold mt-6">Loading...</h2>
         </div>
-        
     );
 };
 
